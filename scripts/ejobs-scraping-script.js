9be92b682594c666294a6da46f279cd89c735d60
sebiflorinp <sebiflorinp@gmail.com>
2025-01-06 17:51:14 +0200

    ADD workflow

--- scripts/ejobs-scraping-script.js
+++ scripts/ejobs-scraping-script.js
@@ -14,7 +14,9 @@ async function scrapeData() {
     
     // Extract the HTML from ejobs
     let url = process.env.EJOBS_LINK
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     
     await page.goto(url)
